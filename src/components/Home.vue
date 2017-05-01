10f6cb2b112ab2e68c152fdab833632a10ce7a28
Shane Richards <shane.richards121@gmail.com>
2017-05-01 08:56:26 -0500

    added dependencies

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>Home</div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>>
+div {
+    background: blue;
+}
+</style>
