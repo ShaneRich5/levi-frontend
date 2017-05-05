7dc2683d65b70f0f50e6baa9a4d86342dba34111
Shane Richards <shane.richards121@gmail.com>
2017-05-05 12:25:09 -0500

    added firebase hosting config

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1>Home</h1>
+  <h1>About</h1>
 </template>
 
 <script>
 export default {
-  name: 'home',
+  name: 'about',
   data() {
     return {
     };
@@ -12,8 +12,5 @@ export default {
 };
 </script>
 
-<style scoped>>
-div {
-  background: blue;
-}
+<style scoped>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import Home from '@/components/Home';
+import About from '@/components/About';
+import Contact from '@/components/Contact';
 
 Vue.use(Router);
 
+const routes = [
+  { path: '/', component: Home, name: 'Home' },
+  { path: '/contact', component: Contact, name: 'Contact' },
+  { path: '/about', component: About, name: 'About' },
+  { path: '/hello', component: Hello, name: 'Hello' },
+];
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
-  ],
+  mode: 'history',
+  routes,
 });
