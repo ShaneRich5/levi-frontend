50987cc71baf7065a15b901dc3c651f44aa7dbd7
iivri.andre <andre_stephe@hotmail.com>
2017-05-16 10:04:49 -0500

    Create sign up form

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,58 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1 style = "font-size : 90px; margin-top: 70px;">{{ message }}</h1>
+
+
+    <md-layout md-align = "center">
+     <md-layout md-tag = "md-card" md-column md-flex = "30" md-flex-medium = "40" md-flex-small = "60"
+      md-flex-xsmall = "90" class = "" >
+
+
+      <md-card-header>
+<div class="md-title">
+ levi
+</div>
+</md-card-header>
+
+
+<md-card-content>
+<md-row>
+
+<md-input-container>
+ <label>First Name</label>
+<md-input />
+</md-input-container>
+
+
+
+<md-input-container>
+  <label>last Name</label>
+<md-input />
+</md-input-container>
+
+
+
+<md-input-container>
+  <label>Email</label>
+<md-input type = "email" required/>
+</md-input-container>
+
+
+<md-input-container>
+  <label> Choose a password</label>
+<md-input type = "password" required/>
+</md-input-container>
+
+</md-row>
+</md-card-content>
+
+
+
+        </md-layout>
+    </md-layout>
   </div>
+
+
 </template>
 
 <script>
@@ -14,7 +65,7 @@ export default {
   },
   data() {
     return {
-      message: 'Homepage',
+      message: 'life Simplified',
     };
   },
 };
