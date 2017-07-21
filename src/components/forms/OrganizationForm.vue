c3022a260768a585d10a0b9a2f1402746bd04994
Shane Richards <shane.richards121@gmail.com>
2017-07-21 04:13:40 -0500

    able to assign types to organizations

--- src/components/forms/OrganizationForm.vue
+++ src/components/forms/OrganizationForm.vue
@@ -26,7 +26,34 @@
         </md-input-container>
       </md-layout>
 
-      <p>{{ organizationTypes }}</p>
+      <md-input-container>
+        <label for="organization-type">Types</label>
+        <md-select
+          name="organization-type"
+          id="movie"
+          v-model="selectedType"
+          @change="addOrganizationType(selectedType)"
+        >
+          <md-option
+            v-for="organizationType in organizationTypes"
+            :key="organizationType.id"
+            :value="organizationType.id"
+          >
+            {{ organizationType.name }}
+          </md-option>
+        </md-select>
+      </md-input-container>
+
+      <div>
+        <md-chip
+          md-deletable
+          v-for="organizationType in organization.types"
+          :key="organizationType.id"
+          @delete="deleteOrganizationType(organizationType.id)"
+        >
+          {{ organizationType.name }}
+        </md-chip>
+      </div>
 
       <md-button
         class="md-primary md-raised"
@@ -48,7 +75,9 @@ export default {
         street: '',
         parish: '',
         country: 'Jamaica',
+        types: [],
       },
+      selectedType: '',
     };
   },
   computed: {
@@ -56,13 +85,34 @@ export default {
   },
   methods: {
     ...mapActions(['createOrganization', 'fetchOrganizationTypes']),
+    addOrganizationType(id) {
+      const selectedType = this.organizationTypes
+        .find(organizationType => organizationType.id === id);
+
+      if (this.organization.types.indexOf(selectedType) === -1) {
+        this.organization.types.push(selectedType);
+      }
+    },
+    deleteOrganizationType(id) {
+      const index = this.organization.types
+        .findIndex(organizationType => organizationType.id === id);
+
+      const types = [...this.organization.types.slice(0, index),
+        ...this.organization.types.slice(index + 1)];
+
+      this.organization.types = types;
+    },
     saveOrganization(organization) {
       if (this.validate(organization)) {
         this.createOrganization(organization);
       }
     },
+
     validate({ name, street, parish }) {
       return name.length !== 0 && street.length !== 0 && parish.length !== 0;
+    },
+    ok(name) {
+      console.log(name);
     },
   },
 };
